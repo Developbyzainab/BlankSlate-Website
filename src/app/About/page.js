728f7c2fb6e32abff974a396801df029271cf894
Developbyzainab <zainabasghar306@gmail.com>
2026-03-08 01:01:37 +0500

    Ready for Vercel deployment

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -5,10 +5,10 @@ import Image from 'next/image'
 export default function page() {
   return (
     <>
-     
-   <section className='w-full min-h-screen relative'>
 
-    <div className='
+      <section className='w-full min-h-screen relative'>
+
+        <div className='
     w-[95%] 
     sm:w-[90%] 
     md:w-[600px] 
@@ -32,7 +32,7 @@ export default function page() {
     lg:p-16
     '>
 
-      <h1 className='
+          <h1 className='
       text-[32px]
       sm:text-[45px]
       md:text-[60px]
@@ -55,10 +55,10 @@ export default function page() {
       bg-white
       pb-3
       '>
-        Our Team Making <br/> the Most of <br/> Digital Era.
-      </h1>
+            Our Team Making <br /> the Most of <br /> Digital Era.
+          </h1>
 
-      <p className='
+          <p className='
       text-xs
       sm:text-sm
       md:text-base
@@ -72,13 +72,13 @@ export default function page() {
       md:mt-10
       lg:mt-15
       '>
-        we help STARTUPS, brands, products and campaigns <br/>
-        WHICH captivate in remarkable ways.
-      </p>
+            we help STARTUPS, brands, products and campaigns <br />
+            WHICH captivate in remarkable ways.
+          </p>
 
-      <span
-        onClick={() => window.scrollTo({ top: window.innerHeight, behavior: "smooth" })}
-        className="
+          <span
+            onClick={() => window.scrollTo({ top: window.innerHeight, behavior: "smooth" })}
+            className="
         absolute 
         right-2
         lg:right-[-5px]
@@ -97,86 +97,159 @@ export default function page() {
         items-center 
         gap-2
         "
-      >
-        SCROLL
-        <span className="w-8 md:w-12 h-[1px] bg-black block"></span>
-      </span>
+          >
+            SCROLL
+            <span className="w-8 md:w-12 h-[1px] bg-black block"></span>
+          </span>
 
-    </div>
-  
-   </section>
-
-
-
-{/* what we are */}
-<section className="w-full justify-center flex flex-col md:flex-row gap-8 px-4 md:px-0 overflow-hidden pb-12">
-
-  {/* text content */}
-  <div className="w-full md:w-[800px] h-auto mt-22 md:pl-32">
-
-    <h1 className="text-5xl md:text-7xl">What we are?</h1>
-
-    <p className="mt-8 md:mt-12 w-full md:w-[530px]">
-      The interest of BlankSlat'e that require work through long-term strategic engagements with clients , typically over the course of multiple years. Through our team of people oriented, excellence focused staff, Blank Slate delivers to take technology from a concept to reality.
-    </p>
-
-    <p className="mt-6 md:mt-8 w-full md:w-[530px]">
-      We provide world-class technology capabilities such as custom software development, app development, complex integrated solutions, modernizing legacy systems, user experience consultancy and cloud computing solutions.
-    </p>
-
-    <p className="mt-6 md:mt-8 w-full md:w-[530px]">
-      We deliver end-to-end services, from strategic design through to full-stack development to a diverse range of projects in Pakistan and Freelance Sites like Upwork, Guru, Fiverr and People Per Hour.
-    </p>
-
-  </div>
-
-  {/* image */}
-  <div className="w-full md:w-[400px] h-auto flex justify-center mr-4 mt-15">
-
-    <Image
-      src="/about.png"
-      alt="about"
-      width={800}
-      height={600}
-      className="w-[340px] md:w-[420px] lg:w-[460px] h-auto"
-    />
-
-  </div>
-
-</section>
-
-
-{/* we make the business */}
-<section className='w-full h-[550px] text-white bg-[#E0246B] pl-30 pt-20 '>
-  <h1 className='text-[90px] leading-none'>We make the business  <br/>
-go BOOM! with topnotch <br/>
-solutions.</h1>
-<div className="relative inline-block mt-8 md:mt-10">
-            <Image
-              src="/circle.png"
-              alt="Button Shape"
-              width={35}
-              height={35}
-              className="absolute -mt-1"
-            />
-            <button className="ml-3">
-              View Portfolio.
-            </button>
-          </div>
-  </section>  
-
-
-
-  {/* our culture */}
-      <section className="w-full justify-center flex flex-col md:flex-row gap-5 px-4 md:px-0 overflow-hidden">
-        <div className="w-full md:w-[520px] h-auto">
-          <h1 className="text-3xl md:text-5xl w-full md:w-[360px] pt-10">OUR CULTURE, VALUES, AND Beliefs.</h1>
         </div>
-        <div className="w-full md:w-[320px] h-auto pt-8">
-          
-            </div>
-           
-        
+
+      </section>
+
+
+
+      {/* what we are */}
+      <section className="w-full justify-center flex flex-col md:flex-row gap-8 px-4 md:px-0 overflow-hidden pb-12">
+
+        {/* text content */}
+        <div className="w-full md:w-[800px] h-auto mt-22 md:pl-32">
+
+          <h1 className="text-5xl md:text-7xl">What we are?</h1>
+
+          <p className="mt-8 md:mt-12 w-full md:w-[530px]">
+            The interest of BlankSlat'e that require work through long-term strategic engagements with clients , typically over the course of multiple years. Through our team of people oriented, excellence focused staff, Blank Slate delivers to take technology from a concept to reality.
+          </p>
+
+          <p className="mt-6 md:mt-8 w-full md:w-[530px]">
+            We provide world-class technology capabilities such as custom software development, app development, complex integrated solutions, modernizing legacy systems, user experience consultancy and cloud computing solutions.
+          </p>
+
+          <p className="mt-6 md:mt-8 w-full md:w-[530px]">
+            We deliver end-to-end services, from strategic design through to full-stack development to a diverse range of projects in Pakistan and Freelance Sites like Upwork, Guru, Fiverr and People Per Hour.
+          </p>
+
+        </div>
+
+        {/* image */}
+        <div className="w-full md:w-[400px] h-auto flex justify-center mr-4 mt-15">
+
+          <Image
+            src="/about.png"
+            alt="about"
+            width={800}
+            height={600}
+            className="w-[340px] md:w-[420px] lg:w-[460px] h-auto"
+          />
+
+        </div>
+
+      </section>
+
+
+      {/* we make the business */}
+      <section className='w-full h-[480px] text-white bg-[#E0246B] pl-30 pt-20 '>
+        <h1 className='text-[80px] leading-none'>We make the business  <br />
+          go BOOM! with topnotch <br />
+          solutions.</h1>
+        <div className="relative inline-block mt-8 md:mt-10">
+          <Image
+            src="/circle.png"
+            alt="Button Shape"
+            width={35}
+            height={35}
+            className="absolute -mt-1"
+          />
+          <button className="ml-3">
+            View Portfolio.
+          </button>
+        </div>
+      </section>
+
+
+
+      {/* our culture */}
+      <section className="w-full justify-center  flex flex-col md:flex-row gap-5 px-4 md:px-0 overflow-hidden">
+        <div className="w-full md:w-[590px] h-70  lg:-ml-20">
+          <h1 className="text-4xl md:text-7xl w-full md:w-[540px] pt-10 ">OUR CULTURE, VALUES, AND Beliefs.</h1>
+        </div>
+        <div className="w-full md:w-[390px] h-415 pt-8  space-y-4 lg:ml-20">
+          <div className='w-full h-50 '>
+            <h1 className='text-2xl'>- pushing boundaries</h1>
+            <p className='text-[17px] mt-5 pl-5'>Blank slate needs to be different. As Thomas Edison once said, “There’s a way to do it better—find it.”  We think your product or service must be innovative in some way if you are to achieve longevity and success.</p>
+
+          </div>
+
+          <div className='w-full h-45 '>
+            <h1 className='text-2xl'>- Rapid Advancement</h1>
+            <p className='text-[17px] mt-5 pl-5'>Blank slate do not simply wait for success to come to them. We plan and prepare for success down to the smallest details, taking brands to the next level.</p>
+
+          </div>
+
+
+           <div className='w-full h-45'>
+            <h1 className='text-2xl'>- Worthy enthusiasm</h1>
+            <p className='text-[17px] mt-5 pl-5'>Blank slate always takes passion over skill because you can teach skills but you cannot teach passion. Our team is passionate about doing significant work in brands we believe in.</p>
+
+          </div>
+
+
+           <div className='w-full h-50'>
+            <h1 className='text-2xl'>- Detail Inspection</h1>
+            <p className='text-[17px] mt-5 pl-5'>Our team exercises extreme attention to details. We’re thorough, accurate, organized, and productive. We believe in paying extra close attention to the smallest details which will make the final product extraordinary. </p>
+
+          </div>
+
+
+           <div className='w-full h-40 '>
+            <h1 className='text-2xl'>- appreciable Traits</h1>
+            <p className='text-[17px] mt-5 pl-5'>Black slate put a lot of emphasis on people with integrity, ethics, and values. We think investing in value oriented person is essential to our growth.</p>
+
+          </div>
+
+
+           <div className='w-full h-45'>
+            <h1 className='text-2xl'>- self driven </h1>
+            <p className='text-[17px] mt-5 pl-5'>Having an entrepreneurial mindset ensures that our employees takes responsibility and accountability for all or most aspects of producing a product or delivering a service.</p>
+
+          </div>
+
+
+
+           <div className='w-full h-45 '>
+            <h1 className='text-2xl'>- Humble attitude</h1>
+            <p className='text-[17px] mt-5 pl-5'>We want to build a thriving business, for that we are always in a state of constant improvement. We never let ego get in the way, we embrace feedback from team members and clients. </p>
+
+          </div>
+
+
+           <div className='w-full h-55'>
+            <h1 className='text-2xl'>- Strong Commitments</h1>
+            <p className='text-[17px] mt-5 pl-5'>Dedication includes a strong sense of support and loyalty to a business or career role. Our team is goal-orientated and is more likely to strengthen any qualities they may need to improve. We value hard work and celebrate accordingly.</p>
+
+          </div>
+        </div>
+
+
+      </section>
+
+
+       {/* we got your back */}
+      <section className='w-full h-[500px] text-white bg-[#0EACDB] pl-30 pt-20 '>
+        <h1 className='text-[80px] leading-none'>We’ve got your back, <br/>
+let’s start today to make <br/>
+something Great!</h1>
+        <div className="relative inline-block mt-8 md:mt-10">
+          <Image
+            src="/circle.png"
+            alt="Button Shape"
+            width={35}
+            height={35}
+            className="absolute -mt-1"
+          />
+          <button className="ml-3">
+            SAY HELLO.
+          </button>
+        </div>
       </section>
     </>
   )
--- src/app/Services/page.js
+++ src/app/Services/page.js
@@ -1,7 +1,7 @@
 "use client";
-
+import Image from 'next/image'
 import React from 'react'
-
+import { Minus } from 'lucide-react';
 
 export default function page() {
   return (
@@ -67,14 +67,14 @@ export default function page() {
       ml-0
       sm:ml-10
       md:ml-30
-      lg:ml-60
+      lg:ml-70
 
       mt-6
       md:mt-10
       lg:mt-15
       '>
         we are proud of the fact that we are ahead of the curve in an ever-evolving industry. <br/>
-        WHICH captivate in remarkable ways.
+        
       </p>
 
       <span
@@ -106,6 +106,262 @@ export default function page() {
     </div>
   
    </section>
+
+
+   {/* what we do */}
+         <section className="w-full justify-center flex flex-col md:flex-row gap-8 px-4 md:px-0 overflow-hidden pb-12">
+   
+           {/* text content */}
+           <div className="w-full md:w-[800px] h-auto mt-22 md:pl-32">
+   
+             <h1 className="text-5xl md:text-7xl">What we do?</h1>
+   
+             <p className="mt-8 md:mt-12 w-full md:w-[530px]">
+               We are here to solve problems and help your business grow. Many things can hold a business back such as repetitive tasks, disconnected systems, and inefficient manual processes. This waste can cost your business valuable resources and prevent you from reaching new customers or worse: risk losing existing ones. Our goal is to give you a solution to these problems and do it on time and on budget.
+             </p>
+   
+             <p className="mt-6 md:mt-8 w-full md:w-[530px]">
+              Unlike other developers, we employ the “agile philosophy” of optimizing software throughout development so that our solution fits your specific needs. For us, “agile” means that we listen to your needs and keep you involved every step of the way from business analysis through to software development and all the way to ongoing support. We want to give you the best possible solution to your problem.
+
+             </p>
+   
+             <p className="mt-6 md:mt-8 w-full md:w-[530px]">
+               We like to stay flexible so that you don’t spend months (or even years) on clunky software that will waste your time. We specialize in doing our jobs quickly and doing them well.     </p>
+   
+           </div>
+   
+           {/* image */}
+           <div className="w-full md:w-[400px] h-auto flex justify-center mr-4 mt-15">
+   
+             <Image
+               src="/service.png"
+               alt="about"
+               width={800}
+               height={600}
+               className="w-[340px] md:w-[420px] lg:w-[360px] h-auto"
+             />
+   
+           </div>
+   
+         </section>
+
+
+
+
+          {/* what we do */}
+               <section className="w-full bg-black text-white py-16 px-4 md:px-20 overflow-hidden">
+                 {/* Top Content */}
+                 <div className="max-w-5xl mx-auto">
+                   <h1 className="text-3xl md:text-6xl">Elevating your brand <br/>
+at every touchpoint.</h1>
+                   <div className="w-full flex flex-col md:flex-row justify-center gap-8 mt-25">
+                   <div className="w-full md:w-73 px-4 md:px-0">
+                     {["CustomIZE Development", "Website Development", "Content Management", "mACHINE lEARNING", "bUSINESS intelligence"].map((text, i) => (
+                       <div key={i} className="flex items-center gap-2 mb-2.5">
+                         <Minus size={16} />
+                         <h1 className="text-[14px] md:text-[16px] font-semibold">{text}</h1>
+                       </div>
+                     ))}
+                   </div>
+                   <div className="w-full md:w-73 px-4 md:px-0">
+                     {["sEO sERVICES", "Research & DEVELOPMENT", "Marketing Material", "Corporate identity", "NATIVE aPP DEVELOPMENT"].map((text, i) => (
+                       <div key={i} className="flex items-center gap-2 mb-2.5">
+                         <Minus size={16} />
+                         <h1 className="text-[14px] md:text-[16px] font-semibold">{text}</h1>
+                       </div>
+                     ))}
+                   </div>
+                   <div className="w-full md:w-73 px-4 md:px-0">
+                     {["UI/UX Design", "gRAPHIC dESIGN", "Motion Graphics", "Cloud Solutions", "iNTERNET oF tHINGS"].map((text, i) => (
+                       <div key={i} className="flex items-center gap-2 mb-2.5">
+                         <Minus size={16} />
+                         <h1 className="text-[14px] md:text-[16px] font-semibold">{text}</h1>
+                       </div>
+                     ))}
+                   </div>
+                 </div>
+                  
+                 </div>
+               </section>
+
+
+                {/* software development*/}
+                     <section className='w-full h-[480px] text-white items-center bg-[#C8DE15] pl-30 pt-10 '>
+                      <Image
+                                  src="/mas2.png"
+                                  alt="l"
+                                  width={700}
+                                  height={500}
+                                  className="w-[50px] md:w-[80px] h-[50px] md:h-[70px] "
+                                />
+                       <h1 className='text-[80px] leading-none px-25'>Software development 
+should solve problems, 
+not create them.</h1>
+                      
+                     </section>
+
+
+
+{/* drastic strategy */}
+
+                    <section className="w-full min-h-screen  py-24 px-6 md:px-16">
+
+  <div className="max-w-7xl mx-auto">
+
+    {/* Heading */}
+    <h1 className="text-5xl md:text-7xl font-light leading-tight max-w-3xl ml-20">
+      Drastic strategy With <br /> Creative Production.
+    </h1>
+
+    {/* Steps */}
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-y-24 gap-x-32 mt-24 ml-20">
+
+      {/* 01 */}
+      <div>
+        <p className="mb-2 text-2xl">01.</p>
+        <h2 className="text-5xl font-medium">Discovering</h2>
+        <p className="text-sm mt-3 max-w-sm">
+          We know when to step in, lead, suggest and leverage our experience for the benefit of the project. Otherwise, what are we doing here?
+        </p>
+      </div>
+
+      <div></div>
+
+      {/* 02 */}
+      <div></div>
+
+      <div>
+        <p className="text-2xl mb-2">02.</p>
+        <h2 className="text-5xl font-medium">Strategizing</h2>
+        <p className="text-sm mt-3 max-w-sm">
+          Nothing beats a perfect plan. We craft the right strategy and give you a roadmap to reach your destination.
+        </p>
+      </div>
+
+      {/* 03 */}
+      <div>
+        <p className="text-2xl mb-2">03.</p>
+        <h2 className="text-5xl font-medium">Execution</h2>
+        <p className="text-sm mt-3 max-w-sm">
+          We are turning all your dreams into reality using creative technology and smart execution.
+        </p>
+      </div>
+
+      <div></div>
+
+      {/* 04 */}
+      <div></div>
+
+      <div>
+        <p className="text-2xl mb-2">04.</p>
+        <h2 className="text-5xl font-medium">Launching</h2>
+        <p className="text-sm mt-3 max-w-sm">
+          We engage customers and employees to help them experience the product through a powerful launch.
+        </p>
+      </div>
+
+      {/* 05 */}
+      <div>
+        <p className="text-2xl mb-2">05.</p>
+        <h2 className="text-5xl font-medium">Evolving</h2>
+        <p className="text-sm mt-3 max-w-sm">
+          In the end it's never finished. Data helps us understand and evolve the product continuously.
+        </p>
+      </div>
+
+    </div>
+
+  </div>
+
+</section>
+
+{/* check out how */}
+
+
+              <section className="w-full bg-[#e9e9e9] py-24 px-6 md:px-20">
+
+  <div className="max-w-7xl mx-auto">
+
+    {/* Heading */}
+    <h1 className="text-5xl md:text-7xl font-light leading-tight max-w-3xl mb-20 ml-17">
+      cHECK oUT how <br/>
+      we can partner <br/>
+      togetheR.
+    </h1>
+
+    {/* Content */}
+    <div className="flex flex-col md:flex-row items-start gap-20 ml-20">
+
+      {/* Image */}
+      <div className="w-full md:w-[420px]">
+        <Image
+          src="/ser2.png"   // apni image ka naam yahan dalna
+          alt="office"
+          width={500}
+          height={600}
+          className="w-full h-150 object-cover"
+        />
+      </div>
+
+      {/* Right Text */}
+      <div className="flex flex-col gap-16 max-w-md mt-30">
+
+        {/* Block 1 */}
+        <div>
+          <h2 className="text-4xl font-medium mb-4">
+            Project-based
+          </h2>
+
+          <p className="text-sm max-w-sm leading-relaxed">
+            We work on project base — to meet the one time need
+            which required more flexible development approach
+            and team collaboration with clear project scope,
+            budget and timeframe.
+          </p>
+        </div>
+
+        {/* Block 2 */}
+        <div>
+          <h2 className="text-4xl font-medium mb-4">
+            Outsourcing
+          </h2>
+
+          <p className="text-sm max-w-sm leading-relaxed">
+            An ideal fit for companies which outsource projects —
+            we work with you to build out the deliverables needed
+            to drive your marketing efforts.
+          </p>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</section>
+
+
+
+ {/* we got your back */}
+      <section className='w-full h-[500px] text-white bg-[#0EACDB] pl-30 pt-20 '>
+        <h1 className='text-[80px] leading-none'>We’ve got your back, <br/>
+let’s start today to make <br/>
+something Great!</h1>
+        <div className="relative inline-block mt-8 md:mt-10">
+          <Image
+            src="/circle.png"
+            alt="Button Shape"
+            width={35}
+            height={35}
+            className="absolute -mt-1"
+          />
+          <button className="ml-3">
+            SAY HELLO.
+          </button>
+        </div>
+      </section>
+   
     </>
   )
 }
